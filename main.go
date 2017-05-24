b3cea499c37da5d94b09c2b2fa9f9738aae20c61
Brian Ho <brian@vandrico.com>
2017-05-24 09:51:34 -0700

    added url to newcontent

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
     "fmt"
     "time"
     "strings"
-    "net/url"
 )
 
 func parseMetalSucks() {
@@ -17,7 +16,7 @@ func parseMetalSucks() {
 
     songs := make([]Song, 0)
 
-    p := ParseEx.ParseEx{
+    p := parseEx.ParseEx{
         Url: "http://metalsucks.net",
         Needle: ".sidebar-reviews article .content-block",
         Middleware: func(i int, s *goquery.Selection) {
@@ -35,18 +34,20 @@ func parseMetalSucks() {
 }
 
 func main() {
+    parseShinto("http://vancouver.singtao.ca/1165366/2017-05-24/post-%E4%B8%80%E4%BB%A3%E4%BF%A0%E5%A5%B3%E4%BA%8E%E7%B4%A0%E7%A7%8B%E9%95%B7%E7%9C%A0%E4%B8%89%E8%97%A9%E5%B8%82/?variant=zh-hk")
 }
 
-func parseShinto() {
-    type Page struct {
-        Url     url.URL
+func parseShinto(strUrl string) {
+
+    type NewsContent struct {
+        Url     string
         Title   string
         Content string
         Created time.Time
     }
 
     p := parseEx.ParseEx{
-        Url: "http://vancouver.singtao.ca/1165366/2017-05-24/post-%E4%B8%80%E4%BB%A3%E4%BF%A0%E5%A5%B3%E4%BA%8E%E7%B4%A0%E7%A7%8B%E9%95%B7%E7%9C%A0%E4%B8%89%E8%97%A9%E5%B8%82/?variant=zh-hk",
+        Url: strUrl,
         Needle: "div#inner_content_ver2",
         Middleware: func(i int, s *goquery.Selection) {
             // parse the Time
@@ -67,7 +68,8 @@ func parseShinto() {
             }
 
             // For each item found, get the band and title
-            page := Page{
+            page := NewsContent{
+                Url: strUrl,
                 Title: strings.TrimSpace(s.Find(".title_noline").Text()),
                 Content: strings.TrimSpace(s.Find(".content").Text()),
                 Created: ctime,
