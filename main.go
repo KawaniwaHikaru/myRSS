19e89182c5cb08fc6efa348f8490c1a99c212df1
Brian Ho <brian@vandrico.com>
2017-05-24 22:14:28 -0700

    rewritten the parse found a bug when doing anonymous with the go rutine

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
     "net/url"
     "path"
     "sync"
+    "regexp"
 )
 
 func parseMetalSucks() {
@@ -38,7 +39,7 @@ func parseMetalSucks() {
     fmt.Println(songs)
 }
 
-func parseShinto(strUrl string) {
+func parseShinto(urlObj url.URL) {
     type NewsContent struct {
         Url     string
         Title   string
@@ -47,7 +48,7 @@ func parseShinto(strUrl string) {
     }
 
     p := parseEx.ParseEx{
-        Url: strUrl,
+        Url: urlObj.String(),
         Needle: "div#inner_content_ver2",
         Middleware: func(i int, s *goquery.Selection) {
             // parse the Time
@@ -71,7 +72,7 @@ func parseShinto(strUrl string) {
 
             // For each item found, get the band and title
             page := NewsContent{
-                Url: strUrl,
+                Url: urlObj.String(),
                 Title: strings.TrimSpace(s.Find(".title_noline").Text()),
                 Content: strings.TrimSpace(content),
                 Created: publishedTime,
@@ -83,83 +84,60 @@ func parseShinto(strUrl string) {
     p.Scan()
 }
 
-func Crawl(strUrl string, srcCh chan string, chFinished chan bool) {
+func parsePage(srcURL string) (links map[string]url.URL) {
 
-    fmt.Println("Fetching Page..")
-    defer func() {
-        fmt.Println("Done Crawling...")
-        chFinished <- true
-    }()
+    // make a hash
+    links = make(map[string]url.URL)
 
-    // main document
-    mainDoc, err := url.Parse(strUrl)
-    if (err != nil) {
-        return
+    srcPageURL, err := url.Parse(srcURL)
+    if err != nil {
+        log.Fatalln("Can not parse URL")
+    }
+
+    // make sure we have schema
+    if srcPageURL.Scheme == "" {
+        srcPageURL.Scheme = "http"
     }
 
     // get the Document
-    resp, err := goquery.NewDocument(strUrl)
+    resp, err := goquery.NewDocument(srcPageURL.String())
     if err != nil {
-        log.Fatalln(`ERROR: Failed to crawl `, strUrl)
+        log.Fatalln(`ERROR: Failed to parse Document `, srcPageURL)
     }
-
 
     // use CSS selector found with the browser inspector
     // for each, use index and item
     resp.Find("a").Each(func(index int, item *goquery.Selection) {
-        //link, _ := item.Find("img").Attr("src")
         link, _ := item.Attr("href")
-        target, _ := item.Attr("target")
-        href, err := url.Parse(link)
+        //linkTarget, _ := item.Attr("target")
 
-        if err != nil || link == "" || link == "#" || target != "_blank" {
+        // see if we can parse this href
+        href, err := url.Parse(link)
+        if err != nil {
+            // can't parse this link, move to next
             return
         }
 
-        if !strings.HasPrefix(link, "http") {
-            link = mainDoc.Scheme + ":" + link
+        // fill in the schema with targetURL
+        if href.Scheme == "" {
+            href.Scheme = srcPageURL.Scheme
         }
 
-        if href.Host == mainDoc.Host {
-            srcCh <- link
+        //filter out empty link or none http links
+        if href.Scheme != "http" || href.Path == "" || href.Path == "#" {
+            return
         }
 
+        // matching for specific path
+        // eg. /1165158/2017-05-24/post-土改三年 逾百萬地主亡/
+        r := regexp.MustCompile(`/(\d*)/(\d{4}-\d{2}-\d{2})/(.*)`)
+        if !r.MatchString(href.Path) {
+            return
+        }
+
+        // all passed, now push it into the hashmap
+        links[href.Path] = *href
     })
-}
-
-
-/**
- Unnecessary complex for parsing one page.
- might as well support multiple init URL
- */
-func parsePage(strUrl string) (links map[string]int) {
-
-    // Channels
-    chLinks := make(chan string)
-    chFinished := make(chan bool)
-
-    // check if we start with http
-    if !strings.HasPrefix(strUrl, "http") {
-        strUrl = "http://" + strUrl
-    }
-
-    // probably want to rewrite this
-    // we are only parsing one page
-    // should do the hash link collection inside
-    // the Crawl
-    go Crawl(strUrl, chLinks, chFinished)
-
-    var isFinished bool
-
-    for !isFinished {
-        select {
-        case url := <-chLinks:
-            links[url]++
-
-        case isFinished = <-chFinished:
-
-        }
-    }
 
     return
 }
@@ -177,18 +155,17 @@ func main() {
     // use a hash-map to hold the found links
     links := parsePage(seedUrls[0])
 
-    // spawn Go Routines for all hash maps
-    // should do some kind of throttling
+    //fmt.Println(len(links))
     wg.Add(len(links))
-    for key := range links {
-        // run the parser and call wg.Done
-        // use Anonymous function to avoid passing the
-        // wg into the function
-        go func() {
-            //fmt.Println(key)
-            parseShinto(key)
+    for _, value := range links {
+
+        // pass the value into the go func,
+        // do not access variable value inside the anonymous function
+        // it is not always pointing to the same object
+        go func(u url.URL) {
+            parseShinto(u)
             wg.Done()
-        }()
+        }(value)
     }
 
     wg.Wait()
